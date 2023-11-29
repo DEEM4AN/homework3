536b87c77a1b95a1c2a84be317e631807ffa2137
Dima Bely <deem4an@gmail.com>
2023-11-29 20:35:53 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,51 @@
 ﻿using System;
-public class Task1 {
-    public static void Main(string [] args) {
+// public class Task1 {
+//     public static void Main(string [] args) {
     
-    int size = 10;
-    Random rnd = new Random();
-    int[] array = new int[size];
+//     int size = 10;
+//     Random rnd = new Random();
+//     int[] array = new int[size];
     
-    for (int i = 0; i < size; i++) 
-    {   
-        array[i] = rnd.Next(1, 100);
-    }
-    Console.WriteLine("количество чисел от 20 до 90: " + numberSegments(array));
-    foreach (var item in array)
-    {
-        Console.Write(item + " ");
-    }
+//     for (int i = 0; i < size; i++) 
+//     {   
+//         array[i] = rnd.Next(1, 100);
+//     }
+//     Console.WriteLine("количество чисел от 20 до 90: " + numberSegments(array));
+//     foreach (var item in array)
+//     {
+//         Console.Write(item + " ");
+//     }
 
-    }
-     public static int numberSegments (int [] arr) {
+//     }
+//      public static int numberSegments (int [] arr) {
+//         int k = 0;
+//         for(int i = 0; i < arr.Length; i++) {
+//             if (arr[i] >= 20 && arr[i] <= 90) {
+//                 k++;
+//             }
+//         }
+//         return k;
+//     }
+// }
+
+public class Task2 {
+    public static void Main(string [] args) {
+        int size = 10;
+        int[] array = new int[size];
+        Random rnd = new Random();
         int k = 0;
-        for(int i = 0; i < arr.Length; i++) {
-            if (arr[i] >= 20 && arr[i] <= 90) {
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = rnd.Next(1, 100);
+            if (array[i] % 2 == 0) {
                 k++;
             }
         }
-        return k;
+
+        Console.WriteLine("количество чётных чисел равно " + k);
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
     }
 }
