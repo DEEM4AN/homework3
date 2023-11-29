95a019d9dbf22f4e52767bcf7914ca6e3cbc8d62
Dima Bely <deem4an@gmail.com>
2023-11-29 21:01:15 +0300

    task3

--- Program.cs
+++ Program.cs
@@ -28,24 +28,44 @@
 //     }
 // }
 
-public class Task2 {
-    public static void Main(string [] args) {
-        int size = 10;
-        int[] array = new int[size];
-        Random rnd = new Random();
-        int k = 0;
-        for (int i = 0; i < size; i++)
+// public class Task2 {
+//     public static void Main(string [] args) {
+//         int size = 10;
+//         int[] array = new int[size];
+//         Random rnd = new Random();
+//         int k = 0;
+//         for (int i = 0; i < size; i++)
+//         {
+//             array[i] = rnd.Next(1, 100);
+//             if (array[i] % 2 == 0) {
+//                 k++;
+//             }
+//         }
+
+//         Console.WriteLine("количество чётных чисел равно " + k);
+//         foreach (var item in array)
+//         {
+//             Console.Write(item + " ");
+//         }
+//     }
+// }
+
+public class Task3 {
+    public static void Main (string[] args) {
+        double[] array = {1.5, 4.7, 3.9, 2.34, 9.31};
+        double min = 99999999;
+        double max = 0;
+        double diff = 0;
+        for (int i = 0; i < array.Length; i++)
         {
-            array[i] = rnd.Next(1, 100);
-            if (array[i] % 2 == 0) {
-                k++;
+            if (array[i] < min) {
+                min = array[i];
+            } else if (array[i] > max) {
+                max = array[i];
             }
         }
+        diff = max - min;
 
-        Console.WriteLine("количество чётных чисел равно " + k);
-        foreach (var item in array)
-        {
-            Console.Write(item + " ");
-        }
+        Console.WriteLine("Разница между самым большим и самым маленьким числом: " + diff);
     }
 }
